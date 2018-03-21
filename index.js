ae446a0ae54c04351b231cf2b214e218de569f6b
ClutteredMind <clutteredmind@users.noreply.github.com>
2018-03-21 16:16:52 -0700

    Added sample auth.json. Added command to set the bot's name.

--- index.js
+++ index.js
@@ -6,8 +6,15 @@
 
 'use strict';
 
+const botName = require('./auth.json').botName;
+
 // create bot
 const bot = require('./discord-client')();
+
+// set bot name
+bot.on('ready', () => {
+    bot.user.setUsername(botName);
+});
 
 // log in
 bot.login(require('./auth.json').discordToken);
